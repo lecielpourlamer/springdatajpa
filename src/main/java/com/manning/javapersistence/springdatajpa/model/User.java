8656ad0a32992f35b87052cf0ffe8f213a14180a
lecielpourlamer <88413718+lecielpourlamer@users.noreply.github.com>
2024-03-12 13:55:58 +0900

    Initial commit

---
+++ src/main/java/com/manning/javapersistence/springdatajpa/model/User.java
@@ -0,0 +1,2 @@
+package com.manning.javapersistence.springdatajpa.model;public class User {
+}
---
+++ src/main/java/com/manning/javapersistence/springdatajpa/repositories/UserRepository.java
@@ -0,0 +1,2 @@
+package com.manning.javapersistence.springdatajpa.repositories;public class UserRepository {
+}
